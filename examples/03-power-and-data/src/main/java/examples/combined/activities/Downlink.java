006808465aa965e87ec8b35a9139acd3c9b06d1e
Dan Delany <daniel.t.delany@jpl.nasa.gov>
2026-08-28 17:39:35 -0700

    add PlaybackData to Downlink in example 03

--- examples/03-power-and-data/src/main/java/examples/combined/activities/Downlink.java
+++ examples/03-power-and-data/src/main/java/examples/combined/activities/Downlink.java
@@ -2,6 +2,8 @@ package examples.combined.activities;
 
 import examples.combined.Mission;
 import examples.combined.SimplePEL.TelecomState;
+import gov.nasa.ammos.plandev.data.activities.PlaybackData;
+
 import gov.nasa.ammos.plandev.contrib.streamline.modeling.discrete.DiscreteEffects;
 import gov.nasa.ammos.plandev.merlin.framework.annotations.ActivityType;
 import gov.nasa.ammos.plandev.merlin.framework.annotations.ActivityType.EffectModel;
@@ -9,11 +11,11 @@ import gov.nasa.ammos.plandev.merlin.framework.annotations.Export.Parameter;
 import gov.nasa.ammos.plandev.merlin.protocol.types.Duration;
 
 import static gov.nasa.ammos.plandev.merlin.framework.ModelActions.delay;
+import static examples.combined.generated.ActivityActions.spawn;
 
 /**
- * Turns on the telecom subsystem for a specified duration.
- * Use alongside PlaybackData (from the data library) to model
- * power draw during data downlink.
+ * Turns on the telecom subsystem and plays back onboard data for the
+ * specified duration.
  */
 @ActivityType("Downlink")
 public class Downlink {
@@ -23,8 +25,12 @@ public class Downlink {
 
   @EffectModel
   public void run(Mission model) {
+    var duration = Duration.of(durationHours, Duration.HOURS);
+
     DiscreteEffects.set(model.pel.telecomState, TelecomState.ON);
-    delay(Duration.of(durationHours, Duration.HOURS));
+    spawn(model, new PlaybackData(duration));
+    delay(duration);
     DiscreteEffects.set(model.pel.telecomState, TelecomState.OFF);
   }
+
 }
